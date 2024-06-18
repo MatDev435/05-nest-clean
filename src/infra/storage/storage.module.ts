3a718c4510ed882c45754bdf883ce7a8f5feac25
MatDev435 <matheuspsalmazo2@gmail.com>
2024-06-18 16:54:11 -0300

    Integração com Cloudflare R2

---
+++ src/infra/storage/storage.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common'
+import { EnvModule } from '../env/env.module'
+import { Uploader } from '@/domain/forum/application/storage/uploader'
+import { R2Storage } from './rs-storage'
+
+@Module({
+  imports: [EnvModule],
+  providers: [
+    {
+      provide: Uploader,
+      useClass: R2Storage,
+    },
+  ],
+  exports: [Uploader],
+})
+export class StorageModule {}
